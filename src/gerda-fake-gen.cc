7ef218d3f649128263706bfd35a5893498fdb554
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-07-08 14:02:45 +0200

    [src] refactor code

--- src/gerda-fake-gen.cc
+++ src/gerda-fake-gen.cc
@@ -12,19 +12,6 @@ namespace logs = utils::logging;
 
 #include "GerdaFactory.h"
 
-#include "json.hpp"
-using json = nlohmann::json;
-
-namespace utils { namespace logging {
-    NLOHMANN_JSON_SERIALIZE_ENUM(utils::logging::level, {
-        {utils::logging::debug,   "debug"},
-        {utils::logging::detail,  "detail"},
-        {utils::logging::info,    "info"},
-        {utils::logging::warning, "warning"},
-        {utils::logging::error,   "error"},
-    })
-}}
-
 int main(int argc, char** argv) {
 
     /*
@@ -90,116 +77,27 @@ int main(int argc, char** argv) {
         );
     }
 
-    // loop over first level of "components"
-    for (auto& it : config["components"]) {
-        // if there's a different path specified at this level
-        auto prefix = it.value("gerda_pdfs", gerda_pdfs);
+    auto comp_list = utils::get_components_json(config);
 
-        /* START INTERMEZZO */
-        // utility to sum over the requested parts (with weight) given isotope
-        auto sum_parts = [&](std::string i) {
-            std::string true_iso = i;
-            if (i.find('-') != std::string::npos) true_iso = i.substr(0, i.find('-'));
-
-            TH1* sum = nullptr;
-            if (it["part"].is_object()) {
-                hist_name = it.value("hist-name", hist_name);
-
-                // compute sum of weights
-                double sumw = 0;
-                for (auto& p : it["part"].items()) sumw += p.value().get<double>();
-
-                for (auto& p : it["part"].items()) {
-                    // get volume name
-                    auto path_to_part = prefix + "/" + p.key();
-                    if (path_to_part.back() == '/') path_to_part.pop_back();
-                    auto part = path_to_part.substr(path_to_part.find_last_of('/')+1);
-                    path_to_part.erase(path_to_part.find_last_of('/'));
-                    auto volume = path_to_part.substr(path_to_part.find_last_of('/')+1);
-                    auto filename = prefix + "/" + p.key() + "/" + true_iso + "/" + "pdf-"
-                        + volume + "-" + part + "-" + i + ".root";
-                    logs::out(logs::debug) << "opening file " << filename << std::endl;
-                    logs::out(logs::debug) << "summing object '" << hist_name << " with weight "
-                                            << p.value().get<double>()/sumw << std::endl;
-                    // get histogram
-                    auto thh = utils::get_component(filename, hist_name, 8000, 0, 8000);
-                    // add it with weight
-                    if (!sum) {
-                        sum = thh;
-                        sum->SetDirectory(nullptr); // please do not delete it when the TFile goes out of scope
-                        sum->Scale(p.value().get<double>()/sumw);
-                    }
-                    else sum->Add(thh, p.value().get<double>()/sumw);
-                }
-                return sum;
-            }
-            else if (it["part"].is_string()) {
-                // get volume name
-                auto path_to_part = prefix + "/" + it["part"].get<std::string>();
-                if (path_to_part.back() == '/') path_to_part.pop_back();
-                auto part = path_to_part.substr(path_to_part.find_last_of('/')+1);
-                path_to_part.erase(path_to_part.find_last_of('/'));
-                auto volume = path_to_part.substr(path_to_part.find_last_of('/')+1);
-                auto filename = prefix + "/" + it["part"].get<std::string>() + "/" + true_iso + "/" + "pdf-"
-                    + volume + "-" + part + "-" + i + ".root";
-                logs::out(logs::debug) << "getting object '" << hist_name << "' in file " << filename << std::endl;
-                // get histogram
-                auto thh = utils::get_component(filename, hist_name, 8000, 0, 8000);
-                return thh;
-            }
-            else throw std::runtime_error("unexpected 'part' value found in \"components\"");
-        };
-        /* END INTERMEZZO */
-
-        // loop over requested isotopes on the relative part
-        for (auto& iso : it["components"].items()) {
-            logs::out(logs::detail) << "building pdf for entry " << iso.key() << std::endl;
-
-            // it's a user defined file
-            if (it.contains("root-file")) {
-                auto filename = it["root-file"].get<std::string>();
-                for (auto& itt : it["components"]) {
-                    auto objname = itt["hist-name"].get<std::string>();
-                    auto th = utils::get_component(filename, objname, 8000, 0, 8000);
-
-                    factory.AddComponent(th, itt["amount-cts"].get<float>());
-                }
-            }
-            else { // look into gerda-pdfs database
-                TH1* comp = nullptr;
-                if (iso.value()["isotope"].is_string()) {
-                    comp = sum_parts(iso.value()["isotope"]);
-                }
-                else if (iso.value()["isotope"].is_object()) {
-                    double sumwi = 0;
-                    for (auto& i : iso.value()["isotope"].items()) sumwi += i.value().get<double>();
-
-                    for (auto& i : iso.value()["isotope"].items()) {
-                        logs::out(logs::debug) << "scaling pdf for " << i.key() << " by a factor "
-                                               << i.value().get<double>()/sumwi << std::endl;
-                        if (!comp) {
-                            comp = sum_parts(i.key());
-                            comp->Scale(i.value().get<double>()/sumwi);
-                        }
-                        else comp->Add(sum_parts(i.key()), i.value().get<double>()/sumwi);
-
-                    }
-                }
-                else throw std::runtime_error("unexpected entry " + iso.value()["isotope"].dump()
-                        + "found in [\"components\"][\"" + iso.key() + "\"][\"isotope\"]");
-
-                factory.AddComponent(comp, iso.value()["amount-cts"].get<float>());
-            }
-        }
-    }
+    // add components to the factory
+    for (auto& e : comp_list) factory.AddComponent(e.hist, e.counts);
 
     logs::out(logs::debug) << "opening output file" << std::endl;
-    auto outname = utils::get_file_obj(config["output-file"].get<std::string>());
+    auto outname = utils::get_file_obj(config["output"]["file"].get<std::string>());
     TFile fout(outname.first.c_str(), "recreate");
 
     // now generate the experiment
+    TH1D hexp(
+        (outname.second != "" ? outname.second : "h").c_str(),
+        "Pseudo experiment",
+        config["output"].value("number-of-bins", 8000),
+        config["output"].value("xaxis-range", std::vector<int>{0, 8000})[0],
+        config["output"].value("xaxis-range", std::vector<int>{0, 8000})[1]
+    );
+    factory.FillPseudoExp(hexp);
+
+    // now generate the experiment
     logs::out(logs::detail) << "filling output histogram" << std::endl;
-    TH1D hexp(outname.second.c_str(), "Pseudo experiment", 8000, 0, 8000);
     factory.FillPseudoExp(hexp);
 
     hexp.Write();
--- src/utils.hpp
+++ src/utils.hpp
@@ -12,10 +12,40 @@
 #include "TF1.h"
 #include "TParameter.h"
 
+#include "json.hpp"
+using json = nlohmann::json;
+
 #ifndef UTILS_HH
 #define UTILS_HH
 
 namespace utils {
+
+    struct bkg_comp {
+        std::string name;
+        TH1* hist; // the struct will own the histogram pointer!
+        float counts;
+
+        // destructor
+        ~bkg_comp() { delete hist; }
+        // constructor
+        bkg_comp(const std::string& n, TH1* h, float c) :
+            name(n), counts(c) {
+            hist = h;
+        }
+        // copy constructor
+        bkg_comp(const bkg_comp& orig) :
+            name(orig.name),
+            counts(orig.counts) {
+            hist = dynamic_cast<TH1*>(orig.hist->Clone());
+        }
+        bkg_comp& operator=(const bkg_comp& orig) {
+            if (this == &orig) return *this;
+            name = orig.name;
+            hist = dynamic_cast<TH1*>(orig.hist->Clone());
+            counts = orig.counts;
+            return *this;
+        }
+    };
 
     // The returned TH1 is owned by the user
     TH1* get_component(std::string filename, std::string objectname, int nbinsx = 100, double xmin = 0, double xmax = 100) {
@@ -72,6 +102,14 @@ namespace utils {
         level min_level = info;
         std::ofstream devnull("/dev/null");
 
+        NLOHMANN_JSON_SERIALIZE_ENUM(utils::logging::level, {
+            {utils::logging::debug,   "debug"},
+            {utils::logging::detail,  "detail"},
+            {utils::logging::info,    "info"},
+            {utils::logging::warning, "warning"},
+            {utils::logging::error,   "error"},
+        })
+
         std::ostream& out(const level& lvl) {
 
             if (lvl == debug and min_level <= debug) {
@@ -96,6 +134,125 @@ namespace utils {
             }
             else return devnull;
         }
+    }
+
+    std::vector<bkg_comp> get_components_json(json& config, std::string gerda_pdfs = "") {
+        std::vector<bkg_comp> comp_map;
+
+        // eventually get a global value for the gerda-pdfs path
+        if (gerda_pdfs == "") gerda_pdfs = config.value("gerda-pdfs", ".");
+        // eventually get a global value for the hist name in the ROOT files
+        auto hist_name = config.value("hist-name", "");
+
+        // loop over first level of "components"
+        for (auto& it : config["components"]) {
+            /* START INTERMEZZO */
+            // utility to sum over the requested parts (with weight) given isotope
+            auto sum_parts = [&](std::string i) {
+                std::string true_iso = i;
+                if (i.find('-') != std::string::npos) true_iso = i.substr(0, i.find('-'));
+
+                TH1* sum = nullptr;
+                if (it["part"].is_object()) {
+                    hist_name = it.value("hist-name", hist_name);
+
+                    // compute sum of weights
+                    double sumw = 0;
+                    for (auto& p : it["part"].items()) sumw += p.value().get<double>();
+
+                    for (auto& p : it["part"].items()) {
+                        // get volume name
+                        auto path_to_part = gerda_pdfs + "/" + p.key();
+                        if (path_to_part.back() == '/') path_to_part.pop_back();
+                        auto part = path_to_part.substr(path_to_part.find_last_of('/')+1);
+                        path_to_part.erase(path_to_part.find_last_of('/'));
+                        auto volume = path_to_part.substr(path_to_part.find_last_of('/')+1);
+                        auto filename = gerda_pdfs + "/" + p.key() + "/" + true_iso + "/" + "pdf-"
+                            + volume + "-" + part + "-" + i + ".root";
+                        logging::out(logging::debug) << "opening file " << filename << std::endl;
+                        logging::out(logging::debug) << "summing object '" << hist_name << " with weight "
+                                                     << p.value().get<double>()/sumw << std::endl;
+                        // get histogram (owned by us)
+                        auto thh = utils::get_component(filename, hist_name, 8000, 0, 8000);
+                        // add it with weight
+                        if (!sum) {
+                            sum = thh;
+                            sum->Scale(p.value().get<double>()/sumw);
+                        }
+                        else {
+                            sum->Add(thh, p.value().get<double>()/sumw);
+                            delete thh;
+                        }
+                    }
+                    return sum;
+                }
+                else if (it["part"].is_string()) {
+                    // get volume name
+                    auto path_to_part = gerda_pdfs + "/" + it["part"].get<std::string>();
+                    if (path_to_part.back() == '/') path_to_part.pop_back();
+                    auto part = path_to_part.substr(path_to_part.find_last_of('/')+1);
+                    path_to_part.erase(path_to_part.find_last_of('/'));
+                    auto volume = path_to_part.substr(path_to_part.find_last_of('/')+1);
+                    auto filename = gerda_pdfs + "/" + it["part"].get<std::string>() + "/" + true_iso + "/" + "pdf-"
+                        + volume + "-" + part + "-" + i + ".root";
+                    logging::out(logging::debug) << "getting object '" << hist_name << "' in file " << filename << std::endl;
+                    // get histogram (owned by us)
+                    auto thh = utils::get_component(filename, hist_name, 8000, 0, 8000);
+                    return thh;
+                }
+                else throw std::runtime_error("unexpected 'part' value found in \"components\"");
+            };
+            /* END INTERMEZZO */
+
+            // loop over the second level of components
+            for (auto& iso : it["components"].items()) {
+                logging::out(logging::debug) << "building PDF for entry " << iso.key() << std::endl;
+
+                // it's a user defined file
+                if (it.contains("root-file")) {
+                    auto filename = it["root-file"].get<std::string>();
+                    auto objname = iso.value()["hist-name"].get<std::string>();
+                    auto th = utils::get_component(filename, objname, 8000, 0, 8000);
+                    th->SetName((iso.key() + "_" + std::string(th->GetName())).c_str());
+
+                    // comp_map now owns the histogram
+                    comp_map.emplace_back(iso.key(), th, iso.value()["amount-cts"].get<float>());
+                }
+                else { // look into gerda-pdfs database
+                    TH1* comp = nullptr;
+                    if (iso.value()["isotope"].is_string()) {
+                        comp = sum_parts(iso.value()["isotope"]);
+                    }
+                    else if (iso.value()["isotope"].is_object()) {
+                        double sumwi = 0;
+                        for (auto& i : iso.value()["isotope"].items()) sumwi += i.value().get<double>();
+
+                        for (auto& i : iso.value()["isotope"].items()) {
+                            logging::out(logging::debug) << "scaling pdf for " << i.key() << " by a factor "
+                                                         << i.value().get<double>()/sumwi << std::endl;
+                            if (!comp) {
+                                comp = sum_parts(i.key());
+                                comp->Scale(i.value().get<double>()/sumwi);
+                            }
+                            else {
+                                auto _tmp = sum_parts(i.key());
+                                comp->Add(_tmp, i.value().get<double>()/sumwi);
+                                delete _tmp;
+                            }
+
+                        }
+                    }
+                    else throw std::runtime_error("unexpected entry " + iso.value()["isotope"].dump()
+                            + "found in [\"components\"][\"" + iso.key() + "\"][\"isotope\"]");
+
+                    // comp_map now owns the histogram
+                    comp->SetName((iso.key() + "_" + std::string(comp->GetName())).c_str());
+                    comp_map.emplace_back(iso.key(), comp, iso.value()["amount-cts"].get<float>());
+                }
+            }
+        }
+
+        return comp_map;
     }
 }
 
