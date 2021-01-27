35b94da1674d9992a6300ac24007f0b702a25ba6
vbiancacci <valentina.biancacci@gmail.com>
2021-01-27 16:26:18 +0100

    only Ar analysis

--- data/Pb214Bi214_distortion.C
+++ data/Pb214Bi214_distortion.C
@@ -5,7 +5,7 @@ void Pb214Bi214_distortion(std::string /*arguments*/) {
     std::string dirname;
     std::vector<TH1*> histos;
 
-    for (auto& s : std::vector<std::string>{"lar/M1_ch0"}) {
+    for (auto& s : std::vector<std::string>{"raw/M1_ch0"}) {
         dirname = s.substr(0, s.find_last_of('/'));
         TFile forig2("gerda-pdfs/ar39-study/tl-reference/cables/cables_all/Pb214/pdf-cables-cables_all-Pb214.root");
         TFile forig1("gerda-pdfs/ar39-study/tl-reference/cables/cables_all/Bi214/pdf-cables-cables_all-Bi214.root");
@@ -13,7 +13,7 @@ void Pb214Bi214_distortion(std::string /*arguments*/) {
         auto horig = dynamic_cast<TH1*>(forig1.Get(s.c_str()));
 
         horig->Add(_horig, 0.3539);
-
+/*
 	TFile fdist2("gerda-pdfs/ar39-study/tl-reference/ge_holders/ge_holders_all/Pb214/pdf-ge_holders-ge_holders_all-Pb214.root");
         TFile fdist1("gerda-pdfs/ar39-study/tl-reference/ge_holders/ge_holders_all/Bi214/pdf-ge_holders-ge_holders_all-Bi214.root");
         auto _hdist = dynamic_cast<TH1*>(fdist2.Get(s.c_str()));
@@ -21,7 +21,7 @@ void Pb214Bi214_distortion(std::string /*arguments*/) {
         hdist->SetDirectory(nullptr);
 
         hdist->Add(_hdist, 0.3539);
-/*	
+	
 	TFile fdist2("gerda-pdfs/ar39-study/tl-reference/minishroud/ms_all/Pb214/pdf-minishroud-ms_all-Pb214.root");
         TFile fdist1("gerda-pdfs/ar39-study/tl-reference/minishroud/ms_all/Bi214/pdf-minishroud-ms_all-Bi214.root");
         auto _hdist = dynamic_cast<TH1*>(fdist2.Get(s.c_str()));
@@ -37,7 +37,7 @@ void Pb214Bi214_distortion(std::string /*arguments*/) {
         hdist->SetDirectory(nullptr);
 
         hdist->Add(_hdist, 0.3539);
-
+*/
         TFile fdist2("gerda-pdfs/ar39-study/tl-reference/larveto/inner_fibers/Pb214/pdf-larveto-inner_fibers-Pb214.root");
         TFile fdist1("gerda-pdfs/ar39-study/tl-reference/larveto/inner_fibers/Bi214/pdf-larveto-inner_fibers-Bi214.root");
         auto _hdist = dynamic_cast<TH1*>(fdist2.Get(s.c_str()));
@@ -45,14 +45,14 @@ void Pb214Bi214_distortion(std::string /*arguments*/) {
         hdist->SetDirectory(nullptr);
 
         hdist->Add(_hdist, 0.3539);
-*/
+
         std::cout << "INFO: computing Pb-214-Bi214 distortion (" << s << "s" << std::endl;
         hdist->Divide(horig);
         hdist->SetName(s.substr(s.find_last_of('/')+1).c_str());
         histos.push_back(hdist);
     }
 
-    auto outname = "distortions/Ar/pdf-Pb214_Bi214-cables_vs_holders.root";
+    auto outname = "distortions/Ar/pdf-Pb214_Bi214-cables_vs_inner_fibers.root";
     std::cout << "INFO: saving to " << outname << std::endl;
     TFile fout(outname, "recreate");
     for (auto& h : histos) {
